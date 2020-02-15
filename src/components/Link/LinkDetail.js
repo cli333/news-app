98820d88e6e44d45b579f0a22e066c547b16b313
cli333 <cml8198@protonmail.com>
2020-02-15 14:28:01 -0800

    added comments

--- src/components/Link/LinkDetail.js
+++ src/components/Link/LinkDetail.js
@@ -1,9 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext, useState } from "react";
+import { FirebaseContext } from "../../firebase";
+import LinkItem from "./LinkItem";
+import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
 
 function LinkDetail(props) {
+  const [link, setLink] = useState(null);
+  const [comment, setComment] = useState("");
+  const { firebase, user } = useContext(FirebaseContext);
   const linkId = props.match.params.linkId;
+  const linkRef = firebase.db.collection("links").doc(linkId);
 
-  return <div>LinkDetail</div>;
+  useEffect(() => {
+    getLink();
+  }, []);
+
+  function getLink() {
+    linkRef.get().then(doc => {
+      setLink({ ...doc.data(), id: doc.id });
+    });
+  }
+
+  function handleAddComment() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      linkRef.get().then(doc => {
+        if (doc.exists) {
+          const previousComments = doc.data().comments;
+          const newComment = {
+            postedBy: {
+              id: user.uid,
+              name: user.displayName
+            },
+            created: Date.now(),
+            text: comment
+          };
+          const updatedComments = [...previousComments, newComment];
+          linkRef.update({ comments: updatedComments });
+          setLink(prevState => ({ ...prevState, comments: updatedComments }));
+        }
+        setComment("");
+      });
+    }
+  }
+
+  return !link ? (
+    <div>Loading...</div>
+  ) : (
+    <div>
+      <LinkItem showCount={false} link={link} />
+      <textarea
+        rows="6"
+        cols="60"
+        onChange={e => setComment(e.target.value)}
+        value={comment}
+      />
+      <div>
+        <button className="button" onClick={handleAddComment}>
+          Add Comment
+        </button>
+      </div>
+      {link.comments.forEach(c => console.log(c))}
+      {link.comments.map((comment, index) => (
+        <div key={index}>
+          <p className="comment-author">
+            {comment.postedBy.name} | {distanceInWordsToNow(comment.created)}
+          </p>
+          <p>{comment.text}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default LinkDetail;
